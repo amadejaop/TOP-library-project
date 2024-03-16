1cb8d8a5b546a14e29643bc56ec507241936bf5e
Amadeja <obsteter.amadeja@gmail.com>
2024-03-16 18:18:29 +0100

    Add a book constructor

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    // function for adding books to the array
+}
+
+// function that displays all of the books on the page
+
+// new book button that brings up a form that allows users to add new books to their library
+
+// remove book button that deletes the book from the array
+
+// change status button that changes the status of the book between read and unread
